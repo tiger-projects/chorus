987f79ff9eea6de2de6f7779d3b533abf73189ce
Saki Reid <saki.reid@gmail.com>
2024-02-19 08:50:50 +0100

    changing some of hte language

--- src/text/translations.tsx
+++ src/text/translations.tsx
@@ -10,10 +10,9 @@ export const translations: Translations = {
 
     home_text: "Connecting Asian Music and Culture with the World",
     mission_text: [
-      "CHORUS is a new ecosystem dedicated to uplifting and connecting Asian music and culture with the world. ",
-      "Founded by renowned Asian executives, CHORUS strives to support Asian artists and their teams to achieve true resonance worldwide. Our vision is to advance the representation and celebration of Asian music from across the continent and diaspora, and elevate its place on the global stage. ",
-      "As a growing team of leaders and experts in music, CHORUS rallies the unmatched insight, expertise and passion of our alliance towards our mission – to champion Asian music and culture, and empower Asian artists, teams and labels to participate in the success, development and growth of Asian music around the world.",
-      "Leveraging this wealth of expertise, Chorus is also uniquely equipped to support international acts from outside of Asia in building authentic and impactful connections with Asian audiences and cultures, and growing these into deep, long-lasting relationships in those markets.",
+      "Chorus is a new ecosystem dedicated to uplifting and connecting Asian music and culture with the world. ",
+      "Founded by renowned Asian executives, Chorus strives to support Asian artists and their teams to achieve true resonance worldwide. Our mission is to advance the representation and celebration of Asian music from across the continent and diaspora, and elevate its place on the global stage. ",
+      "As a growing team of leaders and experts in Asian music, Chorus rallies the unmatched insight, expertise and passion of our alliance towards our collective work: to champion Asian music and culture, and empower Asian artists, teams and labels to participate in the success and development of Asian music around the world. Vice versa, we support artists and teams outside of Asia to build authentic and long-lasting connections with audiences and markets in Asia.",
     ],
     contact_text: [
       "For further enquiries and to request additional details, please email:",
@@ -31,9 +30,8 @@ export const translations: Translations = {
     home_text: "アジアの音楽と文化を世界と繋ぐ",
     mission_text: [
       "Chorusはアジアの音楽と文化を活気づけ、世界と結び付けることに特化した新たなエコシステム。",
-      "数多くの実績で知られ、アジアにルーツを持つメンバーにより設立されたChorusは、アジア系アーティストとそのチームが世界中で真の共感を得るための支援に注力します。私たちのビジョンは、彼らが作り出す音楽の表現と祝福を促進させ、世界規模でその地位を高めることです。",
-      "アジア音楽界のリーダーや専門家で構成され、規模を拡大させているChorusは、チーム一丸となって目指すミッションのために比類ない洞察力、専門知識、情熱を結集させます - 私たちのミッションは、アジアの音楽と文化のために闘い、アジア系アーティスト、チーム、レーベルをエンパワーし、世界中でアジア音楽の成功、発展、成長へ貢献することです。",
-      "Chorusはその幅広い専門性を活かすことで、アジア圏外で活動する海外アーティストがアジアのオーディエンスやカルチャーと真摯でインパクトある繋がりを築くこと、そしてその繋がりをマーケット内で深く、長きに渡る関係へと成長させるための支援も行う。",
+      "数多くの実績で知られ、アジアにルーツを持つメンバーにより設立されたChorusは、アジア系アーティストとそのチームが世界中で真の共感を得るための支援に注力します。私たちのミッションは、彼らが作り出す音楽の表現と祝福を促進させ、世界規模でその地位を高めることです。",
+      "アジア音楽界のリーダーや専門家で構成され、規模を拡大させているChorusは、比類ない洞察力、専門知識、情熱をチーム一丸で取り組むプロジェクトに結集させます。それは、アジアの音楽と文化のために闘い、アジア系アーティスト、チーム、レーベルをエンパワーし、世界中でアジア音楽の成功と発展に貢献することです。私たちはまた、アジア圏以外のアーティストやチームがアジアのオーディエンスやマーケットとの本質的かつ長期的な繋がりを築くためのサポートも行います。",
     ],
     contact_text: [
       "お問い合わせやその他詳細をご希望の方は",

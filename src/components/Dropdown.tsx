86a745c78058c3746c43d6d31d4c4f7f91676e96
Saki Reid <saki.reid@gmail.com>
2024-05-11 12:26:04 +0200

    client list updated

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,61 +1,61 @@
-// import React from "react";
-// import { useGlobalContext } from "../context/languageContext";
-// import { motion, AnimatePresence } from "framer-motion";
+import React from "react";
+import { useGlobalContext } from "../context/languageContext";
+import { motion, AnimatePresence } from "framer-motion";
 
-// interface DropdownProps {
-//   rosterItems: { [key: string]: RosterItem };
-//   isDropdownOpen: boolean;
-//   setDropdownOpen: (isOpen: boolean) => void;
-//   hoveredProjectTitle: string | null;
-//   currentPath: string;
-//   setHoveredProjectTitle: (title: string | null) => void;
-//   darkPaletteStyle: any;
-// }
+interface DropdownProps {
+  rosterItems: { [key: string]: RosterItem };
+  isDropdownOpen: boolean;
+  setDropdownOpen: (isOpen: boolean) => void;
+  hoveredProjectTitle: string | null;
+  currentPath: string;
+  setHoveredProjectTitle: (title: string | null) => void;
+  darkPaletteStyle: any;
+}
 
-// interface RosterItem {
-//   node: {
-//     englishProjectTitle: string;
-//   };
-// }
+interface RosterItem {
+  node: {
+    englishProjectTitle: string;
+  };
+}
 
-// const Dropdown: React.FC<DropdownProps> = ({
-//   isDropdownOpen,
-//   setDropdownOpen,
+const Dropdown: React.FC<DropdownProps> = ({
+  isDropdownOpen,
+  setDropdownOpen,
 
-//   currentPath,
-//   darkPaletteStyle,
-// }) => {
-//   const { getTranslation } = useGlobalContext();
+  currentPath,
+  darkPaletteStyle,
+}) => {
+  const { getTranslation } = useGlobalContext();
 
-//   const handleDropdownToggle = () => {
-//     setDropdownOpen(!isDropdownOpen);
-//   };
+  const handleDropdownToggle = () => {
+    setDropdownOpen(!isDropdownOpen);
+  };
 
-//   return (
-//     <div className={`dropdown ${isDropdownOpen ? "open" : ""}`}>
-//       <motion.a
-//         key={isDropdownOpen ? "open" : "closed"}
-//         href="#"
-//         style={darkPaletteStyle}
-//         id="navbar-main--link"
-//         onClick={handleDropdownToggle}
-//         exit={{ color: "#c8c9c2" }}
-//         initial={{ color: "#c8c9c2" }}
-//         animate={{
-//           color: currentPath === "/" || isDropdownOpen ? "#fff" : "#c8c9c2",
-//           filter: isDropdownOpen
-//             ? "invert(1)"
-//             : currentPath === "/"
-//             ? "invert(0)"
-//             : "invert(1)",
+  return (
+    <div className={`dropdown ${isDropdownOpen ? "open" : ""}`}>
+      <motion.a
+        key={isDropdownOpen ? "open" : "closed"}
+        href="#"
+        style={darkPaletteStyle}
+        id="navbar-main--link"
+        onClick={handleDropdownToggle}
+        exit={{ color: "#c8c9c2" }}
+        initial={{ color: "#c8c9c2" }}
+        animate={{
+          color: currentPath === "/" || isDropdownOpen ? "#fff" : "#c8c9c2",
+          filter: isDropdownOpen
+            ? "invert(1)"
+            : currentPath === "/"
+            ? "invert(0)"
+            : "invert(1)",
 
-//           transition: { delay: 0.1, duration: 0.3, ease: "easeInOut" },
-//         }}
-//       >
-//         {getTranslation("nav_link_two")}
-//       </motion.a>
-//     </div>
-//   );
-// };
+          transition: { delay: 0.1, duration: 0.3, ease: "easeInOut" },
+        }}
+      >
+        {getTranslation("nav_link_two")}
+      </motion.a>
+    </div>
+  );
+};
 
-// export default Dropdown;
+export default Dropdown;
